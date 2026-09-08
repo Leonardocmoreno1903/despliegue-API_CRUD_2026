75efdd549fb935cdabaa132fb457d3d2addc182f
Leonardo Cordero <leonardocmoreno1903@gmail.com>
2026-09-07 23:13:27 -0500

    Correccion de la conexion a MySQL

--- pythonRestApi/ConexionBasededatos.py
+++ pythonRestApi/ConexionBasededatos.py
@@ -56,24 +56,22 @@ class ConexionMysql:
 
     def __init__(self):
         try:
-
             self.mibasededatos = mysql.connector.connect(
                 host=os.getenv("MYSQLHOST"),
-                port=os.getenv("MYSQLPORT"),
+                port=int(os.getenv("MYSQLPORT", "3306")),
                 user=os.getenv("MYSQLUSER"),
                 password=os.getenv("MYSQLPASSWORD"),
-                database=os.getenv("MYSQL_DATABASE")
+                database=os.getenv("MYSQLDATABASE")
             )
 
             self.conexion = self.mibasededatos
             self.cursor = self.conexion.cursor()
 
             if self.mibasededatos.is_connected():
-
-                db_Info = self.mibasededatos.get_server_info()
+                db_info = self.mibasededatos.get_server_info()
 
                 print(
-                    f"CONETADO A MYSQL SERVER, VERSION: {db_Info}"
+                    f"CONECTADO A MYSQL SERVER, VERSION: {db_info}"
                 )
 
         except Error as error:
